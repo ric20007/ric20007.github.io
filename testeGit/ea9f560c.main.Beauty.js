385bc14c670542921e3d5511d080cd81292f055f
Ricardo Mota <ricardo.r.mota@tecnico.ulisboa.pt>
2016-08-28 15:51:42 +0100

    added link to course on exam table, and changed that link style

--- testeGit/ea9f560c.main.Beauty.js
+++ testeGit/ea9f560c.main.Beauty.js
@@ -310,7 +310,12 @@
                     break
                 }
             }
-            O = $("<tr class='color" + i + "'><td>" + e + "<td>" + A + "<td>" + L + "</tr>"), r != null && (O.addClass("clash"), k.children().eq(r).addClass("clash"), s++), d === h.length ? k.append(O) : k.children().eq(d).before(O), h.splice(d, 0, {
+            // CHANGES CHANGES 
+            var href =  '<a href="URL">NAME</a>';
+            href = href.replace("URL", "https://fenix.tecnico.ulisboa.pt/disciplinas/"+e+"/2016-2017/1-semestre");
+            href = href.replace("NAME", e);
+            
+            O = $("<tr class='color" + i + "'><td>" + href + "<td>" + A + "<td>" + L + "</tr>"), r != null && (O.addClass("clash"), k.children().eq(r).addClass("clash"), s++), d === h.length ? k.append(O) : k.children().eq(d).before(O), h.splice(d, 0, {
                 code: e,
                 title: A,
                 time: L,
